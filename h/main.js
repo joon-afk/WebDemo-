28f9f506ca7a9983c4df7fcd6667891bf8719675
joon-afk <dnm0402@naver.com>
2025-03-24 22:58:21 +0900

    as

---
+++ h/main.js
@@ -0,0 +1,32 @@
+document.getElementById("time").innerHTML = new Date().toLocaleString()
+ 
+ function Pink() {
+   document.body.style.backgroundColor = "Pink"
+ }
+ function LightBlue() {
+   document.body.style.backgroundColor = "LightBlue"
+ }
+ LightBlue
+ function Yellow() {
+   document.body.style.backgroundColor = "Yellow"
+ }
+ function Reset() {
+   document.body.style.backgroundColor = "linen"
+ }
+ 
+ function showHTML5() {
+   document.getElementById("fig").src = "스크린샷 2025-03-23 124846.png"
+   document.getElementById("desc").innerHTML = "웹 언어"
+ }
+ function showCSS() {
+   document.getElementById("fig").src = "스크린샷 2025-03-23 124602.png"
+   document.getElementById("desc").innerHTML = "웹 언어"
+ }
+ function showJS() {
+   document.getElementById("fig").src = "스크린샷 2025-03-23 124957.png"
+   document.getElementById("desc").innerHTML = "웹 언어"
+ }
+ function hide() {
+   document.getElementById("fig").src = ""
+   document.getElementById("desc").innerHTML = ""
+ }
